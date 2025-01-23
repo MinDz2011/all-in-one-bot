b129b6a869ecc6e62dddba7dd78b909991669588
MinDz2011 <duykhoa202345@gmail.com>
2025-01-23 16:23:49 +0700

    Update invite.js

--- commands/basic/invite.js
+++ commands/basic/invite.js
@@ -17,7 +17,7 @@ module.exports = {
             .setAuthor({ 
                 name: lang.inviteTitle, 
                 iconURL: lang.inviteImageURL,
-                url: "https://discord.gg/xQF9f9yUEM"
+                url: "https://discord.gg/SPD3F9AwEu"
             })
             .setDescription(lang.inviteDescription.replace('{inviteURL}', inviteURL))
             .setTimestamp();
